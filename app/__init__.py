3ee5aa61ce84b22b0894f6a2bd1d1eca4132ebc1
aashisoni22 <aashi22switzer@gmail.com>
2021-08-10 02:45:11 +0530

    Craete First Project

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///app.db'
+app.config['SECRET_KEY']='243aeed115c195a40757d70a'
+
+db=SQLAlchemy(app)
+bcrypt=Bcrypt(app)
+login_manager=LoginManager(app)
+login_manager.login_view="login_page"
+login_manager.login_message_category="info"
+
+
+from app import routes
+
+
+
